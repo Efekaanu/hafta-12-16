740f856b0769d7bb7ff9496304b433e8b606402d
Efekaanu <155834429+Efekaanu@users.noreply.github.com>
2024-03-15 22:42:09 +0300

    Add files via upload

---
+++ miras.py
@@ -0,0 +1,34 @@
+#class person():
+  #  def __init__(self,fname,lname):
+        #self.fname=fname
+        #self.lname=lname
+ #       print("person oluştu")
+
+#class teacher(person):
+   # def __init__(self,fname,lname,branch):
+   #     self.number=number
+  #  person.__init__(self,fname,lname)
+ #   print ("teacher oluştu")
+
+#p1=person(fname="efe",lname="karaman")
+#print(p1.lname)  
+
+#t1=teacher(fname="fadimana",lname="aladag",branch="sınıf ört")
+#print(t1)
+
+#s1=student(fname="Alex",lname="De Souza",number=123)
+#print(s1)
+
+#p1.ben_kimim()
+
+
+#class işlem():
+ #   def __init__(self,sayi1,sayi2):
+  #      self.sayi1=sayi1
+   #     self.sayi2=sayi2
+    #def sonucu_bul(self):
+     #   pass
+
+
+#class işlem2():
+#    def __init__(self,sayi1,sayi2)
