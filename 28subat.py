ebed562af941d5325fd37b1bf1174e74f0822e90
Efekaanu <155834429+Efekaanu@users.noreply.github.com>
2024-03-15 22:39:35 +0300

    Add files via upload

---
+++ 28subat.py
@@ -0,0 +1,41 @@
+#toplam=5
+#liste=[]
+#for i in range(toplam):
+  #  deger=int(input("yeni değer giriniz: "))
+   # if deger % 3 == 0:
+ #     liste.insert((0,deger))
+  #  else:
+ #       liste.append(deger)
+#print("listenin son hali",liste)
+
+' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '
+' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '
+
+#class kisi:
+    #def __init__(self,name,age):
+    #    self.name=name
+   #     self.age=age
+  #  def __str__(self):
+ #       return f"{self.name} {self.age}"
+#k1=kisi(name="Efe Kaan",age=23)
+#print(k1)
+
+' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '
+' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '
+
+#class soru:
+ #   def __init__(self,soru,cevap):
+  #      self.soru=soru
+   #     self.cevap=cevap
+    # def doğru_mu(self,tahmin):
+     #       return self.cevap==tahmin
+#soru1=soru("başkent neresi: ","ankara")
+#soru2=soru("en kalabalık şehir:","Tokyo")
+#soru3:soru("en güzel şehir:","Sidney")
+#doğru_cevaplar=0
+#sorular=[soru1,soru2,soru3]
+#for i in sorular:
+ #   cevap=input(i.soru)
+  #  if i.dogru_mu(cevap):
+   #     doğru_cevaplar +=1
+#print(f"doğru cevap sayısı {doğru_cevaplar}")
